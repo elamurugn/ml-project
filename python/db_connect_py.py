0750c3af0170f54dc4ef1c29d3d534bef3ae6984
elamurugn <elamurugan365@gmail.com>
2025-09-24 22:07:40 +0530

    Update db_connect_py.py

--- python/db_connect_py.py
+++ python/db_connect_py.py
@@ -1,13 +1,1 @@
-import mysql.connector
 
-def get_connection():
-    """
-    Returns a MySQL database connection object
-    using the same details as your PHP code.
-    """
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",          # empty password as in your PHP code
-        database="tree_oxygen"
-    )
